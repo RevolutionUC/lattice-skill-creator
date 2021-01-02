0d055e22ec83f3ffa55811e36455de093af2f335
Dev Agrawal <devagrawal09@gmail.com>
2021-01-03 03:58:00 +0530

    added csv parsing

--- handler.ts
+++ handler.ts
@@ -1,15 +1,19 @@
 import { APIGatewayEvent, Handler } from 'aws-lambda';
 
 export const uploadSkill: Handler = async (e: APIGatewayEvent) => {
-  // get the skill title and image url from event data
+  // get the csv string from http event data
+
+  // parse csv to get each skill title and image url
+
+  // for each csv entry:
   
-  // download image
+    // download image
 
-  // if image is not svg, resize it to 64x64
+    // if image is not svg, resize it to 64x64
 
-  // upload the image to s3
+    // upload the image to s3
 
-  // create an entry in the database with skill title and url
+    // create an entry in the database with skill title and url
 
   // return 201
 };
